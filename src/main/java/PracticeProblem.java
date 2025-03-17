a89b8116267be9375ad132a4ec9ab627d98af4a8
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-03-17 13:30:04 +0000

    Update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,37 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static boolean validIndex(int[] intArr, int index) {
+		try{
+			int test = intArr[index];
+			return true;
+		}
+		catch (ArrayIndexOutOfBoundsException e){
+				return false;
+		}
+		catch (Exception e){
+			return false;
+		}
 	}
 
-	
 
+	public static int divide(int num, int numTwo){
+		try{
+			int out = num/numTwo;
+			return out;
+	}
+		catch (Exception e){
+			return 0;
+	}
+	}
+	
+	public static int safeConvertStringtoInt(String conver){
+		try {
+			int out = Integer.parseInt(conver);
+			return out;
+		}
+		catch (Exception e){
+			return 0;
+		}
+	}
 }
+	
